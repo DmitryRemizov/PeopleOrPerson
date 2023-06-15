aa451b008251ab8d0645e9301921ef195b420339
Dmitry Remizov <rdk.7777@yahoo.com>
2023-06-15 12:47:12 +0300

    change file 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,6 @@ fun main() {
     print("Введите количество лайков: ")
     val likes = readln().toUInt()
     val comparisonNumber = likes % 10U
-    if (comparisonNumber == 11U || (comparisonNumber % 10U) != 1U) println("Понравилось $likes людям")
+    if (comparisonNumber == 10U || (comparisonNumber % 10U) != 1U) println("Понравилось $likes людям")
     else println("Понравилось $likes человеку")
 }
